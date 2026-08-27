364bf7837f32ad287989b83a41eb5df3b1b3487e
Divyansh <divyansh07020@gmail.com>
2026-08-27 15:22:42 +0530

    Time: 0 ms (100%), Space: 44.5 MB (70.08%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int p = -1, q = -1;
+
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(nums[i] < nums[i + 1]){
+                p = i;
+                break;
+            }
+        }
+
+        if(p == -1){
+            revRange(nums, 0, nums.length - 1);
+            return;
+        }
+
+        for(int i = nums.length - 1; i > p; i--){
+            if(nums[i] > nums[p]){
+                q = i;
+                break;
+            }
+        }
+
+        int temp = nums[p];
+        nums[p] = nums[q];
+        nums[q] = temp;
+
+        revRange(nums, p + 1, nums.length - 1);
+    }
+
+    public void revRange(int[] nums, int start, int end){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}   
