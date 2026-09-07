05a35348b4da703ec1c5cbb4ae23764bca780e0c
Divyansh <divyansh07020@gmail.com>
2026-09-08 00:28:44 +0530

    Time: 16 ms (91.73%), Space: 82.6 MB (67.02%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,29 +1,27 @@
 class Solution {
     public int minDays(int[] bloomDay, int m, int k) {
-        long required = (long)m * k;
-        if(required > bloomDay.length) return - 1;
+        long required = (long) m * k;
+        if(bloomDay.length < required) return -1;
 
-        int low = Integer.MAX_VALUE;
-        int high = Integer.MIN_VALUE;
-        for(int bloom : bloomDay){
-            low = Math.min(low, bloom);
-            high = Math.max(high, bloom);
+        int low = Integer.MAX_VALUE, high = Integer.MIN_VALUE;
+        for(int i : bloomDay){
+            low = Math.min(low, i);
+            high = Math.max(high, i);
         }
 
         while(low <= high){
             int mid = low + (high - low) / 2;
 
-            if(isPossible(bloomDay, m, k, mid)) high = mid - 1;
+            if(canMake(bloomDay, mid, m, k)) high = mid - 1;
             else low = mid + 1;
         }
-        
         return low;
     }
 
-    private boolean isPossible(int[] bloomDay, int m, int k, int day){
+    public boolean canMake(int[] bloomDay, int mid, int m, int k){
         int flowers = 0, bouquets = 0;
-        for(int bloom : bloomDay){
-            if(bloom <= day) flowers++;
+        for(int day : bloomDay){
+            if(day <= mid) flowers++;
             else{
                 bouquets += flowers / k;
                 flowers = 0;
